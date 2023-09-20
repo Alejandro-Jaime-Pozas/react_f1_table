acd343d6ea88c13870adca0e958a6699127e8d45
Alex Jaime <ajaimepozas@gmail.com>
2023-09-20 10:36:29 -0600

    first commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Button(props) {
 
     // The useState function takes one argument, which is the initial value of the state variable. It returns an array with two elements: the current state value and a function to update it
     const [count, setCount] = useState(0)
 
+    // create an effect -> fn to execute after every render
+    useEffect(() => {
+        console.log("useEffect effect callback executed")
+        fetch('https://ergast.com/api/f1/2022/1/driverStandings.json')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    })
+
     function handleClick(e){ // the 'e' refers to the event/Sythetic event, so in this case 'onClick'
         setCount(count + props.step )
-        // console.log('i have been clicked')
+        console.log('button clicked')
     }
 
     return (
