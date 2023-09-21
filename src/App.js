e5f4ea24c265a8db0b632ebba3284be80ceb088a
Alex Jaime <ajaimepozas@gmail.com>
2023-09-21 10:21:13 -0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Button from './components/Button'
 
-export default function App(props) {
+export default function App() {
 
     let buttons = [
         {color: 'primary', step: 1},
@@ -12,18 +12,20 @@ export default function App(props) {
         {color: 'danger', step: 1000},
     ]
 
-    const [count, setCount] = useState(0)
+    // The useState function takes one argument, which is the initial value of the state variable. It returns an array with two elements: the current state value and a function to update it    
     const [racers, setRacers] = useState([])
 
-    // create an effect -> fn to execute after every render
+    // create an effect -> fn to execute after the State is changed
     useEffect(() => {
-        console.log("useEffect effect callback executed");
-    })
-
-    function handleRacerClick(step){
-        console.log('Clicked');
-        setCount(count + step)
-    }
+        console.log("useEffect effect callback executed")
+        fetch('https://ergast.com/api/f1/2022/1/driverStandings.json')
+            .then(res => res.json())
+            .then(data => {
+                let racerStandings = data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+                console.log(racerStandings)
+                setRacers(racerStandings)
+            })
+    }, [])
 
     function handleRacerSubmit(e) {
         e.preventDefault()
@@ -37,13 +39,13 @@ export default function App(props) {
             <Navbar  a={1} name='Brian' city='Chicago' />
             <div className='container'>
                 <h1 className='text-center'>Hello World</h1>
-                <h3 className='text-center'>Total: {count}</h3>
+                <h3 className='text-center'>Total</h3>
                 {buttons.map((b, i) => <Button color={b.color} step={b.step} key={i} />)}
-                <form on onSubmit={handleRacerClick}>
+                <form onSubmit={handleRacerSubmit}>
                     <input type='text' className='form-control' name='season'></input>
                     <input type='submit' value='Submit' ></input>
                 </form>
-                {racers.map((r, i) => <p key={i}>{r}</p>)}
+                {racers.map((r, i) => <p key={i}>{Number(r.points)}</p>)}
             </div>
         </>
     )
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,24 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 export default function Button(props) {
 
-    // The useState function takes one argument, which is the initial value of the state variable. It returns an array with two elements: the current state value and a function to update it
     const [count, setCount] = useState(0)
 
-    // create an effect -> fn to execute after every render
-    useEffect(() => {
-        console.log("useEffect effect callback executed")
-        fetch('https://ergast.com/api/f1/2022/1/driverStandings.json')
-            .then(res => res.json())
-            .then(data => console.log(data))
-    })
-
-    function handleClick(e){ // the 'e' refers to the event/Sythetic event, so in this case 'onClick'
-        setCount(count + props.step )
-        console.log('button clicked')
+    function handleRacerClick(step){
+        console.log('Clicked');
+        setCount(count + step)
     }
 
     return (
-        <button className={`btn btn-${props.color ? props.color : 'primary'} w-100`} onClick={handleClick}>{props.step} - {count}</button>
+        <button className={`btn btn-${props.color ? props.color : 'primary'} w-100`} onClick={() => handleRacerClick(props.step)} >+{props.step} - {count}</button>
         )
 }
