bd95cf4885bcde907fd0ba5f8d7fbfe87c2ca1a8
Alex Jaime <ajaimepozas@gmail.com>
2023-09-26 10:01:54 -0600

    added a Class component, componentDidMount and componentDidUpdate

--- src/components/RacersClass.jsx
+++ src/components/RacersClass.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 
 export default class RacersClass extends Component {
 
+    // 1. component is constructed first
     constructor(props){
         super(props)
+        console.log('racer class constructed');
         // SET THE DIFFERENT STATES FOR EACH VARIABLE NEEDED
         this.state = {
             racers: [],
@@ -12,10 +14,48 @@ export default class RacersClass extends Component {
         }
     }
 
+    // 3. component is mounted after first render of component
+    componentDidMount() {
+        console.log('component mounted')
+        fetch(`https://ergast.com/api/f1/${this.state.season}/${this.state.round}/driverStandings.json`)
+        .then(res => res.json())
+        .then(data => {
+            let racerStandings = data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+            // console.log(racerStandings)
+            this.setState({racers:racerStandings})
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevProps, this.props);
+        console.log(prevState, this.state);
+        if (prevState.round != this.state.round || prevState.season != this.state.season) {
+            fetch(`https://ergast.com/api/f1/${this.state.season}/${this.state.round}/driverStandings.json`)
+            .then(res => res.json())
+            .then(data => {
+                let racerStandings = data.MRData.StandingsTable.StandingsLists[0].DriverStandings
+                // console.log(racerStandings)
+                this.setState({racers:racerStandings})
+        })
+        }
+    }
+
+    handleRacerSubmit = e => {
+        e.preventDefault()
+        console.log(e)
+        let newSeason = e.target.season.value
+        let newRound = e.target.round.value
+        this.setState({
+            season: newSeason,
+            round: newRound
+        })
+    }
+
     // CLASS COMPONENTS USE RENDER() TO RENDER HTML
+    // 2. render method is executed after class construction
+    // 4. render method is executed 2nd time after component is mounted
     render() {
-        console.log(this)
-        console.log(this.props.test)
+        // console.log(this)
         console.log('render method executed')
         let tableHeaders = ['#', 'First', 'Last', 'Points', 'Wins', 'Nationality', 'Constructor' ]
 
@@ -23,7 +63,7 @@ export default class RacersClass extends Component {
         return (
             <div className='row py-5'>
             <h4 className="text-center">Driver Standings</h4>
-            <form onSubmit={handleRacerSubmit}>
+            <form onSubmit={this.handleRacerSubmit}>
                 <div className="row">
                     <div className="col-12 col-md-6 my-2">
                         <input className='form-control' type="text" name="season" placeholder="Enter Season" />
